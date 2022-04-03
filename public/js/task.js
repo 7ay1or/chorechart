35cb470bd035d287cfbb2b304d44fc55bda41f52
Zack Panula <1053951+zpanula@users.noreply.github.com>
2022-04-03 14:58:58 -0400

    Refactor renderTasks into TaskAPI class method

--- public/js/task.js
+++ public/js/task.js
@@ -33,20 +33,21 @@ class TaskAPI {
       .then((res) => res.json())
       .then((res) => console.log(res));
   }
-}
 
-function renderTasks() {
-  let tasklist = document.getElementById("tasklist");
+  static getTasks() {
+    let tasklist = document.getElementById("tasklist");
 
-  fetch("/api/tasks")
-    .then((res) => res.json())
-    .then((data) => {
-      data.map((task) => {
-        let li = document.createElement("li");
-        li.className = "list-group-item";
-        li.textContent = task.name;
-        tasklist.append(li);
+    fetch("/api/tasks")
+      .then((res) => res.json())
+      .then((data) => {
+        data.map((task) => {
+          let li = document.createElement("li");
+          li.className = "list-group-item";
+          li.textContent = task.name;
+          tasklist.append(li);
+        });
       });
-    });
+  }
 }
-renderTasks();
+
+TaskAPI.getTasks();
